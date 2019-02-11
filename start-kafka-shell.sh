14cbdbca37a4d6054ca83c7d0f2202bd167a682a
rodneystar <rodneystar66@gmail.com>
2019-02-11 23:04:58 +0000

    add start producer and start consumer scripts

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -w /opt/kafka_2.12-2.1.0 -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka:2.12_2.1.0 /bin/bash 
